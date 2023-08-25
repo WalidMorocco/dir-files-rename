af8050a0eeafbe0ed9324caba7f7b406b907b0ec
Walid Amar <walidamarpro@gmail.com>
2023-08-25 04:15:10 -0400

    rename subdirectory files added

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
 
 def rename_files_in_directory(directory_path, old_char, new_char):
-    # List all files in the directory
-    for filename in os.listdir(directory_path):
-        # Check if filename contains the character/string to be replaced
-        if old_char in filename:
-            # Create the new filename by replacing old_char with new_char
-            new_filename = filename.replace(old_char, new_char)
-            # Get the full path for both old and new filenames
-            old_file_path = os.path.join(directory_path, filename)
-            new_file_path = os.path.join(directory_path, new_filename)
-            # Rename the file
-            os.rename(old_file_path, new_file_path)
-            print(f"Renamed {filename} to {new_filename}")
+    # Walk through directory_path, including its subdirectories
+    for dirpath, dirnames, filenames in os.walk(directory_path):
+        for filename in filenames:
+            # Check if filename contains the character/string to be replaced
+            if old_char in filename:
+                # Create the new filename by replacing old_char with new_char
+                new_filename = filename.replace(old_char, new_char)
+                # Get the full path for both old and new filenames
+                old_file_path = os.path.join(dirpath, filename)
+                new_file_path = os.path.join(dirpath, new_filename)
+                # Rename the file
+                os.rename(old_file_path, new_file_path)
+                print(f"Renamed {old_file_path} to {new_file_path}")
 
 if __name__ == "__main__":
     dir_path = input("Enter the directory path: ")
